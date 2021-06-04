cd9ad02c8c1642142127f7a885f61b292d38d20f
ihollander <ianholla@gmail.com>
2021-06-04 11:32:26 -0400

    Solution

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,17 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, except: [:created_at, :updated_at], methods: [:summary]
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheese
+    else
+      render json: { error: "Cheese not found" }, status: :not_found
+    end
   end
 
 end
