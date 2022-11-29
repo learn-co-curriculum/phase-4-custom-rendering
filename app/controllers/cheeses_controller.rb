c28a4ff0a37a8467fb1a59f929f36a803360553b
Doreen <doreenlulu97@gmail.com>
2022-11-29 14:08:24 +0300

    commit tree 1828bcb4316264d4b194ecafe754130c969b62ce

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -2,14 +2,23 @@ class CheesesController < ApplicationController
 
   # GET /cheeses
   def index
+    # cheeses = Cheese.all
+    # render json: cheeses, only [:id, :name, :price, :is_best_seller]
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, except: [:created_at, :updated_at]
   end
 
   # GET /cheeses/:id
   def show
-    cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    # cheese = Cheese.find_by(id: params[:id])
+    # render json: cheese
+      cheese = Cheese.find_by(id: params[:id])
+      render json: {
+      id: cheese.id,
+      name: cheese.name,
+      price: cheese.price,
+      is_best_seller: cheese.is_best_seller
+  }
   end
 
 end
