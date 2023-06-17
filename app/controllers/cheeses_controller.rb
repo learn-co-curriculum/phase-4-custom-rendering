f802cd519405a43e9f9d2455b23bb22a06ed95da
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-06-17 07:54:50 -0700

    custom rendering

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,25 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, only: [:id, :name, :price, :is_best_seller], methods: [:summary]
+
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheeses, only: [:id, :name, :price, :is_best_seller], methods: [:summary]
+    else
+      render json: { error: 'Cheese not found' }, status: :not_found
+    end
+
+
+    # render json: cheese
+  end
+  
+  def summary 
+    "#{name}: $#{price}"
   end
 
 end
